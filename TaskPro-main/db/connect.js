37c79c6ebce871a0c2e18d88c2fe8aadeca3b125
VECHA JATADHAR <146861280+Jatadhar16@users.noreply.github.com>
2025-10-05 17:41:51 +0530

    Delete TaskPro-main directory

--- TaskPro-main/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "config/.env" });
-
-const connectDB =()=>{
-mongoose.connect(process.env.DB_LINK);
-console.log("connected to DB..."); 
-} 
-module.exports = connectDB;
-  
--- TaskPro-main/errorMiddleWare/404-not-found.js
+++
@@ -1,5 +0,0 @@
-
-const notFound = (req, res) => res.status(404).send("Route does not exist");
-// no use
-console.log("not found");
-module.exports = notFound; 
